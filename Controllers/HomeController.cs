6a48b8ccd72e8c7832e7a59c0d478d228106241a
Timofey Teterin <timofei.teterin@inbox.ru>
2023-03-16 09:37:34 +0300

    using mssql connection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using dotnet_mvc.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
 
 namespace dotnet_mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public IConfiguration _configuration { get; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
-        }
+            _configuration = configuration;
+        } 
 
         public IActionResult Index()
         {
+            string connection_string = _configuration.GetConnectionString("DefaultConnection");
+
+            SqlConnection sqlConnection = new SqlConnection(connection_string);
+            sqlConnection.Open();
+            SqlCommand testSqlCommand = new SqlCommand("SELECT COUNT(*) FROM mvc_table", sqlConnection);
+            int count = (int) testSqlCommand.ExecuteScalar();
+
+            ViewData["TotalData"] = count;
+
+            sqlConnection.Close();
+
             return View();
         }
 
