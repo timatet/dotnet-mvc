12d66664fecf9a33ab4dbd5cc16fa6a6dd4e9854
Timofey Teterin <timofei.teterin@inbox.ru>
2023-05-07 00:05:10 +0300

    Html-level access rights

--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.Identity;
 using dotnet_mvc.Models.Auxiliary;
 using dotnet_mvc.Helpers;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace dotnet_mvc
 {
@@ -55,38 +56,67 @@ namespace dotnet_mvc
             .AddDefaultTokenProviders();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext dbContext)
-        {
-            dbContext.Database.Migrate();
+        public static async Task Initialize(
+            ApplicationDbContext applicationDbContext, 
+            UserManager<UserModel> userManager, 
+            RoleManager<UserRoleModel> roleManager
+        ){
+            applicationDbContext.Database.Migrate();
 
-            if (!dbContext.Users.ToList().Exists(user => user.UserName == "admin")) {
-                dbContext.Users.Add(
-                    new UserModel() {
-                        UserName = "admin",
-                        FullName = "Administratior",
-                        IsAdmin = true
-                    }
-                );
+            // Create roles
+            // string[] roles = new string[] { "UserManager", "StaffManager" };
+            // foreach (string role in roles)
+            // {
+            //     if (!await roleManager.RoleExistsAsync(role))
+            //     {
+            //         await roleManager.CreateAsync(new IdentityRole(role));
+            //     }
+            // }
 
-                dbContext.SaveChanges();
+            // Create admin user
+            if (!applicationDbContext.Users.ToList().Exists(u => u.UserName == "admin"))
+            {
+                UserModel userModel = new UserModel();
+                userModel.UserName = "admin";
+                userModel.Email = "admin@timatet.ru";
+                userModel.IsAdmin = true;
+                userModel.EmailConfirmed = true;
+                var umod = await userManager.CreateAsync(userModel, "Admin78");
             }
 
-            if (env.IsDevelopment())
+            // Ensure admin privileges
+            // ApplicationUser admin = await userManager.FindByEmailAsync("info@example.com");
+            // foreach (string role in roles)
+            // {
+            //     await userManager.AddToRoleAsync(admin, role);
+            // }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder applicationBuilder, 
+            IWebHostEnvironment webHostEnvironment, 
+            ApplicationDbContext applicationDbContext, 
+            UserManager<UserModel> userManager, 
+            RoleManager<UserRoleModel> roleManager
+        ){
+            Initialize(applicationDbContext, userManager, roleManager).Wait();
+
+            if (webHostEnvironment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                applicationBuilder.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                applicationBuilder.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                applicationBuilder.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            applicationBuilder.UseHttpsRedirection();
+            applicationBuilder.UseStaticFiles();
             
             /* Организация доступа к `node_modules` по адресу `/vendor` из `_Layout.cshtml` */
-            app.UseStaticFiles(new StaticFileOptions{
+            applicationBuilder.UseStaticFiles(new StaticFileOptions{
                 FileProvider = new PhysicalFileProvider(Path.Combine(
                     Directory.GetCurrentDirectory(),
                     "node_modules"
@@ -94,12 +124,12 @@ namespace dotnet_mvc
                 RequestPath = new PathString("/vendor")
             });
 
-            app.UseRouting();
+            applicationBuilder.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+            applicationBuilder.UseAuthentication();
+            applicationBuilder.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            applicationBuilder.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
