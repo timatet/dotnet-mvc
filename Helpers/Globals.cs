ce1633df73b7dd864f2477449bc8c25f988f7cc2
Timofey <timofei.teterin@inbox.ru>
2023-10-03 17:58:36 +0300

    Обновлен Dockerfile/Globals

--- Helpers/Globals.cs
+++ Helpers/Globals.cs
@@ -8,10 +8,11 @@ namespace dotnet_mvc.Helpers
     public static class Globals
     {
         public static string RELEASE_VERSION = "DEV";
-        public static string DATABASE_SERVER = "localhost";
-        public static string DATABASE_NAME = "mvc";
-        public static string DATABASE_USERNAME = "sa";
-        public static string DATABASE_PASSWORD = "None";
+        
+        public static string DATABASE_SERVER;
+        public static string DATABASE_NAME;
+        public static string DATABASE_USERNAME;
+        public static string DATABASE_PASSWORD;
 
         public static void Initialize() {
             RELEASE_VERSION = Environment.GetEnvironmentVariable("RELEASE_VERSION_ENVIRONMENT");
