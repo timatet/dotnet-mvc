d9c0dfb2f31ce0bf4a25f067ce3c493aff55ca92
Timofey Teterin <timofei.teterin@inbox.ru>
2023-10-03 12:01:07 +0300

    Database updated

---
+++ Migrations/20231003085706_RemovedIsAdminField.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_mvc.Migrations
+{
+    public partial class RemovedIsAdminField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
